4cb8b941fe7b1b16d66ccdd22a0622f88f5e6ff3
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-12 01:31:05 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,27 +1,28 @@
 class Patient
 
-  attr_accessor :name, :appointment, :doctor
-  @@all = []
+    @@all = []
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
-  def self.all
-    @@all
-  end
+    def self.all
+        @@all
+    end
 
-  def appointments
-    Appointment.all.select {|appointment| appointment.patient == self}
-  end
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
 
-  def doctors
-    appointment.collect {|appointment| appointment.doctor}
-  end
+    def appointments
+        Appointment.all.select { |appt| appt.patient == self }
+        #interate thru Appointments array & retn those belonging to self
+    end
 
-  def new_appointment(doctor, date)
-    Appointment.new(self, date, doctor)
-  end
+    def doctors
+        self.appointments.map { |appt| appt.doctor }
+        #iterate over self.appointments & collect docotres belong to each appt
+    end
 
-end 
+end
